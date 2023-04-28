bbc62104f32554de3d5cccf10c7fbd81a9efb16a
Cole K <cole-k@users.noreply.github.com>
2023-04-27 21:46:27 -0700

    flux-snapshot auto commit (no message)

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,10 @@ mod union_find;
 #[flux::trusted]
 fn main() {
     let mut uf = init_union_find(10);
-    println!("root of 0: {}", find_root(&mut uf, 0));
-    println!("root of 3: {}", find_root(&mut uf, 3));
+    union(&mut uf, 0, 1);
+    union(&mut uf, 1, 2);
+    union(&mut uf, 8, 9);
+    assert!(find_root(&mut uf, 0) == find_root(&mut uf, 2));
+    assert!(find_root(&mut uf, 0) != find_root(&mut uf, 8));
+    assert!(find_root(&mut uf, 8) == find_root(&mut uf, 9));
 }
