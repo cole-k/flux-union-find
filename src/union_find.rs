71fcc2279c676f7f52afb97e89f624d2800e1f14
Cole K <cole-k@users.noreply.github.com>
2023-04-27 21:31:33 -0700

    flux-snapshot auto commit (no message)

--- src/union_find.rs
+++ src/union_find.rs
@@ -18,6 +18,18 @@ pub struct UnionFind {
     nodes: Vec<UFNode>,
 }
 
+#[flux::trusted]
+#[flux::sig(fn(&UnionFind[@size], usize{v: v <= size}) -> &UFNode[size])]
+fn get_node(uf: &UnionFind, index: usize) -> &UFNode {
+    uf.nodes.get(index).unwrap()
+}
+
+#[flux::trusted]
+#[flux::sig(fn(&mut UnionFind[@size], usize{v: v <= size}) -> &mut UFNode[size])]
+fn get_mut_node(uf: &mut UnionFind, index: usize) -> &mut UFNode {
+    uf.nodes.get_mut(index).unwrap()
+}
+
 #[flux::sig(fn(usize[@size]) -> UnionFind[size])]
 pub fn init_union_find(size: usize) -> UnionFind {
     let mut nodes = Vec::with_capacity(size);
@@ -36,11 +48,11 @@ pub fn init_union_find(size: usize) -> UnionFind {
 
 #[flux::sig(fn(&mut UnionFind[@size], node_index: usize{v: v <= size}) -> usize{v: v <= size})]
 pub fn find_root(uf: &mut UnionFind, node_index: usize) -> usize {
-    let parent_index = uf.nodes[node_index].parent_index;
+    let parent_index = get_node(uf, node_index).parent_index;
     if parent_index == node_index {
         return node_index;
     }
     let root = find_root(uf, parent_index);
-    uf.nodes[node_index].parent_index = root;
+    get_mut_node(uf, node_index).parent_index = root;
     return root;
 }
